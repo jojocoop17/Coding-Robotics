c81ebe119f41878b50459a992c7a6dab93b24aa1
jojocoop17 <50964986+jojocoop17@users.noreply.github.com>
2019-05-23 11:43:51 -0700

    Add files via upload

---
+++ sketch_oct19a_RAMP_BOE_BOT.ino
@@ -0,0 +1,51 @@
+
+
+#include <Servo.h>                           // Include servo library
+
+
+
+
+
+Servo servoRight;
+Servo servoLeft;                             // Declare left and right servos
+
+ 
+void setup()                                 // Built-in initialization block
+{
+     Servo servoRight;
+Servo servoLeft;                           // Delay to finish tone
+
+  servoLeft.attach(13);                      // Attach left signal to pin 13
+  servoRight.attach(12);                     // Attach right signal to pin 12
+
+  // Full speed forward
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(2000);                               // ...for 2 seconds
+// Turn right in place
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1450);        // Right wheel counterclockwise
+  delay(1500);                                // ...for 0.6 seconds
+
+  // Turn left in place
+  servoLeft.writeMicroseconds(1550);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(3000);                                // ...for 0.6 seconds
+
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1450);        // Right wheel counterclockwise
+  delay(1400);    
+ 
+  // Full speed forward
+  servoLeft.writeMicroseconds(1700);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel counterclockwise
+  delay(2000);                               // ...for 2 seconds
+
+  servoLeft.detach();                        // Stop sending servo signals
+  servoRight.detach();
+}  
+
+void loop()                                  // Main loop auto-repeats
+{                                            // Empty, nothing needs repeating
+}
+
